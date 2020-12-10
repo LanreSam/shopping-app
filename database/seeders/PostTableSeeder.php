ae1a7f4665deec975cb365e8debe9e603848c911
LanreSam <ezechelanre@gmail.com>
2020-12-10 07:53:23 -0600

    seeders

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class PostTableSeeder extends Seeder
 {
@@ -14,9 +15,12 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            'title' => 'Third post title',
-            'body' => 'Third post body'
-        ]);
+        $faker = Faker::create();
+        foreach (range(1,100) as $index) {
+            DB::table('posts')->insert([
+                'title' => $faker->sentence(5),
+                'body' => $faker->paragraph(4)
+            ]);
+        }
     }
 }
