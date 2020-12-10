ab3830e0710f33b7df72c6341a8fca8091d4eaba
LanreSam <ezechelanre@gmail.com>
2020-12-10 11:03:38 -0600

    blade template

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,7 @@
                 <h1>Contact Page Content</h1>
                 <ul>
                     <li><a href="/home">Home</a></li>
-                    <li><a href="/about">contact</a></li>
+                    <li><a href="/about">About</a></li>
                 </ul>
             </div>
         </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
             <div class="col-md-12">
                 <h1>Home Page Content</h1>
                 <ul>
-                    <li><a href="/about">Home</a></li>
+                    <li><a href="/about">About</a></li>
                     <li><a href="/contact">contact</a></li>
                 </ul>
             </div>
