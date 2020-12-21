65d0c25580327d91c2e87f3209addc60abd3eb7e
LanreSam <ezechelanre@gmail.com>
2020-12-21 09:52:59 +0100

    new commit

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -7,7 +7,7 @@
     <title>Users</title>
 
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
 rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
@@ -32,9 +32,8 @@ rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbD
                         @endforeach
                     </tbody>
                 </table>
-                <div class="d-flex justify-content-center">
-                    {!! $users->links() !!}
-                </div>
+                {{ $users->links() }}
+
             </div>
         </div>
     </div>
