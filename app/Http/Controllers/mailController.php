296013073f15a556ac28fb058688f03a973d53fa
LanreSam <ezechelanre@gmail.com>
2020-12-23 02:45:18 +0100

    sending mails

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\testMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class mailController extends Controller
 {
-    //
+    public function sendMails()
+    {
+        $details = [
+            'title' => 'This is a Mail from me to myself',
+            'body' => 'This is for testing how mail works in laravel'
+        ];
+
+        Mail::to('ezechelanre@gmail.com')->send(new TestMail($details));
+        return "Email sent";
+    }
 }
