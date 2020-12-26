0b64e0142850d9e4a1f9d9074a414ef410812086
LanreSam <ezechelanre@gmail.com>
2020-12-26 13:51:47 +0100

    still working on gmail

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -8,31 +8,31 @@ use Illuminate\Support\Facades\Mail;
 
 class mailController extends Controller
 {
-    // public function sendMails()
-    // {
-    //     $details = [
-    //         'title' => 'This is a Mail from me to myself',
-    //         'body' => 'This is for testing how mail works in laravel'
-    //     ];
-
-    //     Mail::to('ezechelanre@gmail.com')->send(new testMail($details));
-    //     return "Email sent";
-    // }
-
     public function sendMails()
     {
-        //$to_name = "Lanre";
-        $to_email = "ezechelanre@gmail.com";
-        $data = [
-            'name'=>'Lanre',
-            'body' => 'A test email'
+        $details = [
+            'title' => 'This is a Mail from me to myself',
+            'body' => 'This is for testing how mail works in laravel'
         ];
-        Mail::send('emails.mail', $data, function($message) use ($to_email) {
 
-            $message->to($to_email)->subject("Laravel Test Mail");
-            $message->from("ezechelanre@gmail.com");
-
-        });
+        Mail::to('ezechelanre@gmail.com')->send(new testMail($details));
+        return "Email sent";
     }
+
+    // public function sendMails()
+    // {
+    //     //$to_name = "Lanre";
+    //     $to_email = "ezechilanre@gmail.com";
+    //     $data = [
+    //         'name'=>'Lanre',
+    //         'body' => 'A test email'
+    //     ];
+    //     Mail::send('emails.mail', $data, function($message) use ($to_email) {
+
+    //         $message->to($to_email)->subject("Laravel Test Mail");
+    //         $message->from("ezechelanre@gmail.com");
+
+    //     });
+    // }
 
 }
