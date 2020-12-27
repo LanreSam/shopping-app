915551004c66cca92262ae44c909bdfda9484eaa
LanreSam <ezechelanre@gmail.com>
2020-12-27 18:42:34 +0100

    one-to-one eloquent CRUD operation

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Phone extends Model
 {
     use HasFactory;
+
+    protected $table = "phones";
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\Users');
+    }
 }
